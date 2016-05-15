cd45f783c025a699a6c6284f1990001754e1754c
Gyusun Yeom <omniavinco@gmail.com>
2016-05-15 11:07:41 +0900

    ignore unknown pronounciation chinese characters

--- jsaheui.js
+++ jsaheui.js
@@ -103,7 +103,12 @@ function dan_gye(han_beon){ //step; han_beon means whether it executes a single 
 		c = code_gonggan[y].charCodeAt(x);
 		if((c >= 0x4e00 && c <= 0x9fff) || (c >= 0x3400 && c <= 0x4DBF) || (c >= 0x20000 && c <= 0x2CEAF)){
 			c = chinese_to_kr[c];
-			c = c.charCodeAt(Math.floor(Math.random() * c.length));
+			if(c){
+				c = c.charCodeAt(Math.floor(Math.random() * c.length));
+			}else{
+				cursor_omgyeo();
+				continue;
+			}
 		}else if(c < 0xac00 || c > 0xd7a3){
 			cursor_omgyeo();
 			continue;
